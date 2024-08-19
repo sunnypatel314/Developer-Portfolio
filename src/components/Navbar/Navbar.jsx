243211925e51b81bccce0d5003f2a0e40de13f63
Sunny Patel <sunnypatel4prez@gmail.com>
2024-08-18 20:23:40 -0500

    Readme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,6 +41,9 @@ const Navbar = () => {
           <li>
             <a href="#contact">Contact</a>
           </li>
+          {/* <li>
+            <a>Resume</a>
+          </li> */}
         </ul>
       </div>
     </nav>
